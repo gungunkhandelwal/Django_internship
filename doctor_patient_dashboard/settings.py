87482d05a439d2dd3d140eb129b672b6afb7ed6b
gungunkhandelwal <khandelwalgungun525@gmail.com>
2024-07-09 13:58:37 +0530

    Final commit

--- doctor_patient_dashboard/settings.py
+++ doctor_patient_dashboard/settings.py
@@ -133,8 +133,10 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'accounts', "static"),
+    os.path.join(BASE_DIR,"static"),
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
