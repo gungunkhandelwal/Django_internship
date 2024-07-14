550bfc67e946b51df2d7429cdebbab6e21745517
gungunkhandelwal <khandelwalgungun525@gmail.com>
2024-07-14 14:25:59 +0530

    Last commit

--- doctor_patient_dashboard/settings.py
+++ doctor_patient_dashboard/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'PORT':env('DATABASE_PORT')
     }
 }
-database_url=os.environ.get("DATABASE_URL")
+database_url=env("DATABASE_URL")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
