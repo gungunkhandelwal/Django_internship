0979cdf0afa150ba4bc397f4e8ce8fb68cd23424
gungunkhandelwal <khandelwalgungun525@gmail.com>
2024-07-14 17:12:25 +0530

    setting.py

--- doctor_patient_dashboard/settings.py
+++ doctor_patient_dashboard/settings.py
@@ -27,7 +27,6 @@ environ.Env.read_env(os.path.join(BASE_DIR,'.env'))  #Reading .env
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =os.environ.get("SECRET_KEY")
-# SECRET_KEY='9d8c6c8408be79af6baeb9f65ac2992d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
